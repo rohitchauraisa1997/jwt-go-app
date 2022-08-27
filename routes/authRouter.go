b59f91f342cbc862aae93f525166d1bb28b1597f
rohitchauraisa1997 <rohit.chaurasia997@gmail.com>
2022-08-27 19:36:58 +0900

    auth and user router

---
+++ routes/authRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controller "jwt-go-app/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "jwt-go-app/controllers"
+	"jwt-go-app/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
